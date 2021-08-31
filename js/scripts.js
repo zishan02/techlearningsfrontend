bf5b76f01b12a2f083610f74c3a0b7625e53f910
Zishan <zishanmohammed02@gmail.com>
2021-09-01 01:07:15 +0530

    added ajax support

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,9 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+$(document).ready(function(){
+  $.get("https://techlearningsbackend.herokuapp.com/fetchPost", function(data){
+    $("#postdescr").text(data[0].postData);
+    });
+
+});
