3b1a5c40ed5627d35ab928c5ae6ce30ebaedf28d
Zishan <zishanmohammed02@gmail.com>
2021-12-04 22:53:25 +0530

    added admin ui

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,11 @@ $(document).ready(function(){
   $.get("https://techlearningsbackend.herokuapp.com/fetchPost", function(data){
 //  console.log(JSON.stringify(data));
     $("#postdescr1").text(data[0].postPreview);
-    $("#postId").val(data[0].postId);
+    $("#postId1").val(data[0].postId);
+     $("#postId2").val(data[1].postId);
+      $("#postId3").val(data[2].postId);
+       $("#postId4").val(data[3].postId);
+        $("#postId5").val(data[4].postId);
      $("#postdescr2").text(data[1].postPreview);
       $("#postdescr3").text(data[2].postPreview);
        $("#postdescr4").text(data[3].postPreview);
@@ -21,31 +25,32 @@ $(document).ready(function(){
       $("#postTitle5").text(data[3].postTitle);
     });
 $("#postTitle1").click(function (event) {
-localStorage.setItem('id', $("#postId").val());
+alert($("#postId1").val());
+localStorage.setItem('id', $("#postId1").val());
 
 window.location.href='blogtemplate.html';
 
 });
 $("#postTitle2").click(function (event) {
-localStorage.setItem('id', $("#postId").val());
+localStorage.setItem('id', $("#postId2").val());
 
 window.location.href='blogtemplate.html';
 
 });
 $("#postTitle3").click(function (event) {
-localStorage.setItem('id', $("#postId").val());
+localStorage.setItem('id', $("#postId3").val());
 
 window.location.href='blogtemplate.html';
 
 });
 $("#postTitle4").click(function (event) {
-localStorage.setItem('id', $("#postId").val());
+localStorage.setItem('id', $("#postId4").val());
 
 window.location.href='blogtemplate.html';
 
 });
 $("#postTitle5").click(function (event) {
-localStorage.setItem('id', $("#postId").val());
+localStorage.setItem('id', $("#postId5").val());
 
 window.location.href='blogtemplate.html';
 
